299010a18ff22dd3069b43caf9e2b2945ca3b507
ksteimel <ksteimel.umail.iu.edu>
2017-10-20 23:04:19 -0400

    Added tests for sentence spliting

---
+++ test/sentences.jl
@@ -0,0 +1,16 @@
+module TestSentenceTok
+	using Base.Test
+	using Languages
+	using TestAnalysis
+	using Compat
+	
+	sample_text = """\"Colorless green ideas sleep furiously\". Noam Chomsky's famous quote is emblazoned upon the minds of linguists the world over. What does this expression actually means?"
+	"""
+
+	sentences = TextAnalys.sentencetok(Languages.EnglishLanguage, sample_text)
+
+	@assert isequal(
+		tkns,
+		Compat.UTF8String["\"Colorless green ideas sleep furiously\"", "Noam Chomsky's famous quote is emblazoned upon the minds of linguists the world over.", "What does this expression actually mean?"]
+	)
+end
