c55259a97a81c6e59705918928e2a9a4dd0ff946
ksteimel <ksteimel.umail.iu.edu>
2017-11-10 23:23:24 -0500

    Added test for tokenizer with punctuation splitting

--- test/tokenizer.jl
+++ test/tokenizer.jl
@@ -15,4 +15,17 @@ module TestTokenizer
         tkns,
         Compat.UTF8String["this", "is", "some", "sample", "text"]
     )
+
+    #Testing punctuation separation 
+    sample_text2 = "This is some more text. This text is similar to the last."
+    
+    tknsSplitPunct = TextAnalysis.tokenize(
+        Languages.EnglishLanguage,
+        sample_text2,
+        splitPunct=true
+    )
+    @assert isequal(
+        tknsSplitPunct,
+        Compat.UTF8String["This", "is", "some", "more", "text", ".", "This", "text", "is", "similar", "to", "the", "last", "."]
+    )
 end
